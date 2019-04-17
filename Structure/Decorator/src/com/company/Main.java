e594e6afa4a6eb9a078519451aa8aefb83dfdd6d
Valery Kalugin <kalugin.1912@gmail.com>
2019-04-17 23:23:34 +0300

    Decorator

--- Structure/Decorator/src/com/company/Main.java
+++ Structure/Decorator/src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String text = "Text one two three six";
+        DataSource baseDataSource = new DataSourceDecorator(new FileDataSource("out/OutputDemo.txt"));
+        DataSource encryptionSource = new CompressionDecorator(new EncryptionDecorator(baseDataSource));
+        encryptionSource.writeData(text);
+        System.out.println(encryptionSource.readData());
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        System.out.println("- Input ----------------");
+        System.out.println(text);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encryptionSource.readData());
+
+
     }
 }
